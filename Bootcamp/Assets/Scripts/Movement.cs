508dd09dc594a90986c67d82b6dbfff0be00b420
Divyansh Joshi <57811359+archrave@users.noreply.github.com>
2020-12-08 23:50:13 +0530

    A small force is added in the front direction, in the start function to the player object.

---
+++ Bootcamp/Assets/Scripts/Movement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Rigidbody x;
+    void Start()
+    {
+        x.AddForce(0, 200, 500);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
