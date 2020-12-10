258d4659fde8780b6861e2fa060ca67a9674aafc
Divyansh Joshi <57811359+archrave@users.noreply.github.com>
2020-12-10 16:36:27 +0530

    Added Follow Camera Script

---
+++ New Bootcamp/Assets/Scripts/CamMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody r;
+    public Vector3 offset;
+    public Camera cam;
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+    private void FixedUpdate() {
+
+     cam.transform.position = r.transform.position + offset;   
+    }
+}
---
+++ New Bootcamp/Assets/Scripts/Movement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+    //Change "Movement" 
+{
+    public float fwdForce = 1000f;
+    public float sideForce = 500f;
+    public Rigidbody x;
+    void Start()
+    {
+<<<<<<< HEAD
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        x.AddForce(0, 0, 1000 * Time.deltaTime);
+        if(Input.GetKey("d"))
+        {
+            x.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+        else if(Input.GetKey("a"))
+        {
+            x.AddForce(-500 * Time.deltaTime, 0, 0);
+=======
+        x = GetComponent<Rigidbody>();
+        
+    }
+
+    private void FixedUpdate() {
+        x.AddForce(0, 0, fwdForce*Time.deltaTime);
+        if(Input.GetKey("a"))
+        {
+            x.AddForce(-sideForce, 0,0);
+        }
+        if(Input.GetKey("d"))
+        {
+            x.AddForce(sideForce,0,0);
+>>>>>>> 3949779b4d1cd5de15c8b830681c95989ac806a5
+        }
+    }
+}
